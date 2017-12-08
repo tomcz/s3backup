47f5c4a4b8f88fe8f139c5609a9e9472ab7d9ba7
David Cohen <david@cohen24.com>
2017-12-08 13:40:37 -0500

    Update client.go

--- src/s3backup/client/client.go
+++ src/s3backup/client/client.go
@@ -1,5 +1,32 @@
 package client
+/*
+Hi Tom,
+The work you’ve done is incredibly impressive—you clearly know your stuff. 
+I’ve always been impressed with the engineers that come out of Thoughtworks.  
+I’m looking to build out a core services team that will leverage python as its primary language, and I need
+an experienced person like you on the team to help guide our direction. 
+I’m looking for an architect who will roll up their sleeves and dig into the code, but will still take time 
+to mentor some of the more junior engineers who have gotten Aaptiv to where we are today.
 
+
+A little bit about our company: Aaptiv is a rapidly growing consumer fitness startup and leader in the digital fitness arena. 
+We’ve raised over $32M in financing and are nearing 200k paying subscribers, more users than nearly any digital fitness company 
+on the market. 
+Forbes just did a piece too: https://www.forbes.com/sites/alexkonrad/2017/11/30/how-aaptiv-reached-20-mil-and-raised-more/#5f922c8150e9.
+
+On the services side, the near future has us working on things like a commerce service to integrate payment vendors, 
+an authentication service and a content service to surface all of our workouts.
+
+If you’re interested, hit me up… Would love to chat and have you talk to some of the team.
+
+Talk soon,
+David Cohen
+David@aaptiv.com
+VP Engineering - AAptiv
+https://www.linkedin.com/in/davidcohen24/
+
+
+*/
 import (
 	"log"
 	"os"
