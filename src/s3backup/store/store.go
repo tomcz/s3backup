9b5c00e5b0f80ccd68425e70097e968f2a3a033f
Tom Czarniecki <tomcz@users.noreply.github.com>
2018-01-31 02:21:02 -0500

    let's make it explicit that the returned string is the checksum

--- src/s3backup/store/store.go
+++ src/s3backup/store/store.go
@@ -2,5 +2,5 @@ package store
 
 type Store interface {
 	UploadFile(remotePath, localPath, checksum string) error
-	DownloadFile(remotePath, localPath string) (string, error)
+	DownloadFile(remotePath, localPath string) (checksum string, err error)
 }
