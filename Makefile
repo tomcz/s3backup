908660ca58625bf8740eae3c5ccb0176b5454088
Tom Czarniecki <tomcz@users.noreply.github.com>
2018-04-07 23:50:41 -0400

    switch to dep for dependency management

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
-GITCOMMIT = $(shell git rev-parse --short HEAD 2>/dev/null)
-GOPATH = $(shell git rev-parse --show-toplevel 2>/dev/null)
-LDFLAGS = -X s3backup/version.commit=${GITCOMMIT}
+GITCOMMIT := $(shell git rev-parse --short HEAD 2>/dev/null)
+BASE_DIR := $(shell git rev-parse --show-toplevel 2>/dev/null)
+LDFLAGS := -X app/s3backup/version.commit=${GITCOMMIT}
+GO_PATH := ${BASE_DIR}/code
 
 precommit: clean format test build
 
-travis: clean test build
+travis: clean deps test build
+
+deps:
+	cd ${GO_PATH}/src/app && GOPATH=${GO_PATH} dep ensure
 
 clean:
 	rm -rf target
@@ -13,12 +17,12 @@ target:
 	mkdir target
 
 format:
-	GOPATH=${GOPATH} go fmt s3backup/...
+	GOPATH=${GO_PATH} go fmt app/s3backup/...
 
 test:
-	GOPATH=${GOPATH} go test -cover -ldflags "${LDFLAGS}" s3backup/...
+	GOPATH=${GO_PATH} go test -cover -ldflags "${LDFLAGS}" app/s3backup/...
 
-compile = GOPATH=${GOPATH} GOOS=$2 GOARCH=amd64 go build -ldflags "${LDFLAGS}" -o target/$1-$2 s3backup/cmd/$1
+compile = GOPATH=${GO_PATH} GOOS=$2 GOARCH=amd64 go build -ldflags "${LDFLAGS}" -o target/$1-$2 app/s3backup/cmd/$1
 
 build: target
 	$(call compile,s3backup,linux)
---
+++ code/src/app/s3backup/client/client_test.go
@@ -0,0 +1,89 @@
+package client
+
+import (
+	"testing"
+
+	"app/s3backup/mocks"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetRemoteFileWithoutDecryption(t *testing.T) {
+	hash := &mocks.Hash{}
+	store := &mocks.Store{}
+
+	c := &Client{
+		Hash:  hash,
+		Store: store,
+	}
+
+	store.On("DownloadFile", "s3://foo/bar.txt", "bar.txt").Return("muahahaha", nil)
+	hash.On("Verify", "bar.txt", "muahahaha").Return(nil)
+
+	assert.NoError(t, c.GetRemoteFile("s3://foo/bar.txt", "bar.txt"))
+
+	hash.AssertExpectations(t)
+	store.AssertExpectations(t)
+}
+
+func TestGetRemoteFileWithDecryption(t *testing.T) {
+	hash := &mocks.Hash{}
+	store := &mocks.Store{}
+	cipher := &mocks.Cipher{}
+
+	c := &Client{
+		Hash:   hash,
+		Store:  store,
+		Cipher: cipher,
+	}
+
+	store.On("DownloadFile", "s3://foo/bar.txt", "bar.txt.tmp").Return("muahahaha", nil)
+	hash.On("Verify", "bar.txt.tmp", "muahahaha").Return(nil)
+	cipher.On("Decrypt", "bar.txt.tmp", "bar.txt").Return(nil)
+
+	assert.NoError(t, c.GetRemoteFile("s3://foo/bar.txt", "bar.txt"))
+
+	hash.AssertExpectations(t)
+	store.AssertExpectations(t)
+	cipher.AssertExpectations(t)
+}
+
+func TestPutLocalFileWithoutEncryption(t *testing.T) {
+	hash := &mocks.Hash{}
+	store := &mocks.Store{}
+
+	c := &Client{
+		Hash:  hash,
+		Store: store,
+	}
+
+	hash.On("Calculate", "bar.txt").Return("woahahaha", nil)
+	store.On("UploadFile", "s3://foo/bar.txt", "bar.txt", "woahahaha").Return(nil)
+
+	assert.NoError(t, c.PutLocalFile("s3://foo/bar.txt", "bar.txt"))
+
+	hash.AssertExpectations(t)
+	store.AssertExpectations(t)
+}
+
+func TestPutLocalFileWithEncryption(t *testing.T) {
+	hash := &mocks.Hash{}
+	store := &mocks.Store{}
+	cipher := &mocks.Cipher{}
+
+	c := &Client{
+		Hash:   hash,
+		Store:  store,
+		Cipher: cipher,
+	}
+
+	cipher.On("Encrypt", "bar.txt", "bar.txt.tmp").Return(nil)
+	hash.On("Calculate", "bar.txt.tmp").Return("woahahaha", nil)
+	store.On("UploadFile", "s3://foo/bar.txt", "bar.txt.tmp", "woahahaha").Return(nil)
+
+	assert.NoError(t, c.PutLocalFile("s3://foo/bar.txt", "bar.txt"))
+
+	hash.AssertExpectations(t)
+	store.AssertExpectations(t)
+	cipher.AssertExpectations(t)
+}
