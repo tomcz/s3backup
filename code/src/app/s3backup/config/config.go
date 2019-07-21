d421ab49516ce4c86430d74ef9ac6f39034f2ffe
Tom Czarniecki <tomcz@users.noreply.github.com>
2019-07-21 11:42:03 -0400

    describe purpose and requirements of config parameters

--- code/src/app/s3backup/config/config.go
+++ code/src/app/s3backup/config/config.go
@@ -1,10 +1,10 @@
 package config
 
 type Config struct {
-	CipherKey   string `mapstructure:"cipher_key"`
-	S3AccessKey string `mapstructure:"s3_access_key"`
-	S3SecretKey string `mapstructure:"s3_secret_key"`
-	S3Token     string `mapstructure:"s3_token"`
-	S3Region    string `mapstructure:"s3_region"`
-	S3Endpoint  string `mapstructure:"s3_endpoint"`
+	CipherKey   string `mapstructure:"cipher_key"`    // Optional but highly recommended, 256-bit AES key, Base64-encoded
+	S3AccessKey string `mapstructure:"s3_access_key"` // Mandatory, Access Key ID
+	S3SecretKey string `mapstructure:"s3_secret_key"` // Mandatory, Secret Access Key
+	S3Token     string `mapstructure:"s3_token"`      // Optional, depends on your AWS configuration
+	S3Region    string `mapstructure:"s3_region"`     // Mandatory, without it AWS operations will fail
+	S3Endpoint  string `mapstructure:"s3_endpoint"`   // Optional, will use AWS defaults if not present
 }
