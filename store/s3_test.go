897ffa798f5bb51ee8f8d725fbb843dbd6a366e5
Tom Czarniecki <tomcz@users.noreply.github.com>
2019-09-15 11:53:26 -0400

    ensure that checksum is truly optional

--- store/s3_test.go
+++ store/s3_test.go
@@ -35,7 +35,7 @@ func TestSplitRemotePath(t *testing.T) {
 	assert.Error(t, err)
 }
 
-func TestRoundTripUploadDownload(t *testing.T) {
+func TestRoundTripUploadDownload_withChecksum(t *testing.T) {
 	backend := s3mem.New()
 	faker := gofakes3.New(backend)
 	ts := httptest.NewServer(faker.Server())
@@ -69,5 +69,42 @@ func TestRoundTripUploadDownload(t *testing.T) {
 	require.NoError(t, err, "Cannot read downloaded file")
 
 	assert.Equal(t, "wibble", checksum)
+	assert.Equal(t, expected, actual, "File contents are different")
+}
+
+func TestRoundTripUploadDownload_withoutChecksum(t *testing.T) {
+	backend := s3mem.New()
+	faker := gofakes3.New(backend)
+	ts := httptest.NewServer(faker.Server())
+	defer ts.Close()
+
+	expected, err := tools.Random(4096)
+	require.NoError(t, err, "Cannot create file contents")
+
+	uploadFile, err := tools.CreateTempFile("upload", expected)
+	require.NoError(t, err, "Cannot create file to upload")
+	defer os.Remove(uploadFile)
+
+	accessKey := "AKIAIOSFODNN7EXAMPLE"
+	secretKey := "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+	client, err := NewS3(accessKey, secretKey, "", "us-east-1", ts.URL)
+	require.NoError(t, err, "failed to create S3 client")
+
+	store := client.(*s3store)
+	_, err = store.client.CreateBucket(&s3.CreateBucketInput{Bucket: aws.String("test-bucket")})
+	require.NoError(t, err, "failed to create bucket")
+
+	err = client.UploadFile("s3://test-bucket/test-file", uploadFile, "")
+	require.NoError(t, err, "failed to upload file")
+
+	downloadFile := uploadFile + ".download"
+	checksum, err := client.DownloadFile("s3://test-bucket/test-file", downloadFile)
+	require.NoError(t, err, "failed to download file")
+	defer os.Remove(downloadFile)
+
+	actual, err := ioutil.ReadFile(downloadFile)
+	require.NoError(t, err, "Cannot read downloaded file")
+
+	assert.Equal(t, "", checksum)
 	assert.Equal(t, expected, actual, "File contents are different")
 }
