bcdf50de11e83c09c917b73e78ea38cff88915a5
Tom Czarniecki <tomcz@users.noreply.github.com>
2022-11-12 09:41:09 +1100

    comment cleanup

--- client/cipher.go
+++ client/cipher.go
@@ -1,8 +1,8 @@
 package client
 
+//go:generate mockgen --source=cipher.go --destination=mocks/cipher.go --package=mocks
+
 type Cipher interface {
 	Encrypt(plainTextFile, cipherTextFile string) error
 	Decrypt(cipherTextFile, plainTextFile string) error
 }
-
-//go:generate mockgen --source=cipher.go --destination=mocks/cipher.go --package=mocks
--- client/hash.go
+++ client/hash.go
@@ -1,8 +1,8 @@
 package client
 
+//go:generate mockgen --source=hash.go --destination=mocks/hash.go --package=mocks
+
 type Hash interface {
 	Calculate(filePath string) (string, error)
 	Verify(filePath, expectedChecksum string) error
 }
-
-//go:generate mockgen --source=hash.go --destination=mocks/hash.go --package=mocks
