59986f46ff50ee89167abb0de279b23315725194
Tom Czarniecki <tomcz@users.noreply.github.com>
2025-10-27 15:22:36 +1100

    stop vendoring deps

--- vendor/github.com/alecthomas/kong/guesswidth.go
+++
@@ -1,9 +0,0 @@
-//go:build tinygo || appengine || (!linux && !freebsd && !darwin && !dragonfly && !netbsd && !openbsd)
-
-package kong
-
-import "io"
-
-func guessWidth(w io.Writer) int {
-	return 80
-}
